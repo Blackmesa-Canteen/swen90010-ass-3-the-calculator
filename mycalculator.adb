821c91efe719646412c457f2999575b6e3dff516
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2023-05-12 14:58:36 +1000

    Almost finish implementation

--- mycalculator.adb
+++ mycalculator.adb
@@ -26,7 +26,6 @@ package body MyCalculator with SPARK_Mode is
     -- try unlock. 
     procedure Unlock(C : in out MyCalculator; PINString : in String)is
     begin
-
         -- check whether the string is valid
         if not IsPin(PINString) then
             raise PIN_Exception with "PIN should be 0000 . . . 9999.";
@@ -42,7 +41,6 @@ package body MyCalculator with SPARK_Mode is
     -- try lock
     procedure Lock(C : in out MyCalculator; PINString : in String) is
     begin
-
         -- check whether the string is valid
         if not IsPin(PINString) then
             raise PIN_Exception with "PIN should be 0000 . . . 9999. ";
@@ -87,13 +85,6 @@ package body MyCalculator with SPARK_Mode is
         if (C.isLocked = True) then
             raise Lock_Exception with "Calculator is locked.";
         else
-            -- handle int overflow: Decimal integers in commands 
-            -- (e.g. “5” in “push 5”) that are outside the range 
-            -- −2^31. . . 2^31-1 inclusive are treated as 0.
-            if not IsValidInt(NumIn) then
-                NumIn := 0;
-            end if;
-
             NumOut := C.storage(C.size);
             C.size := C.size - 1;
         end if;
@@ -205,5 +196,104 @@ package body MyCalculator with SPARK_Mode is
             end;      
         end if;
     end PerformOperation;
+
+
+    -- For a string var, the procedure loads the value stored 
+    -- in variable var and pushes it onto the stack.
+    procedure loadVar(C : in out MyCalculator; VarString: in String) is
+    begin
+        -- check lock state
+        if (C.isLocked = True) then
+            raise Lock_Exception with "Calculator is locked.";
+        else
+            -- check whether the string is valid
+            if not IsValidVarName(VarString) then
+                raise Var_Exception with "Variable name is invalid.";
+            end if;
+
+            declare
+                V : VariableStore.Variable;
+                Num : Item;
+            begin
+                -- check whether the variable exists
+                V := VariableStore.From_String(VarString);
+                if not VariableStore.Has_Variable(C.variableDB, V) then
+                    raise Var_Exception with "Variable does not exist.";
+                else
+                    -- If exist, push the value onto the stack
+                    Num := VariableStore.Get(C.variableDB, V);
+                    PushNumber(C, Num);
+                end if;
+            end;
+            
+        end if;
+    end loadVar;
+
+    -- pops the value from the top of the stack and stores it 
+    -- into variable var, defining that variable if it is not already defined.
+    procedure storeVar(C : in out MyCalculator; VarString: in String) is
+    begin
+        -- check lock state
+        if (C.isLocked = True) then
+            raise Lock_Exception with "Calculator is locked.";
+        else
+            -- check whether the string is valid
+            if not IsValidVarName(VarString) then
+                raise Var_Exception with "Variable name is invalid.";
+            end if;
+
+            declare
+                V : VariableStore.Variable;
+                Num : Item;
+            begin
+                -- pop the value from the top of the stack
+                PopNumber(C, Num);
+
+                -- store the value into variable var
+                V := VariableStore.From_String(VarString);
+                VariableStore.Put(C.variableDB, V, Num);
+            end;
+        end if;
+    end storeVar;
+
+    -- makes variable var undefined (i.e. it will not be printed by subsequent “list” commands).
+    procedure removeVar(C : in out MyCalculator; VarString: String) is 
+    begin
+        -- check lock state
+        if (C.isLocked = True) then
+            raise Lock_Exception with "Calculator is locked.";
+        else
+            -- check whether the string is valid
+            if not IsValidVarName(VarString) then
+                raise Var_Exception with "Variable name is invalid.";
+            end if;
+
+            declare
+                V : VariableStore.Variable;
+            begin
+                -- check whether the variable exists
+                V := VariableStore.From_String(VarString);
+                if not VariableStore.Has_Variable(C.variableDB, V) then
+                    raise Var_Exception with "Variable does not exist.";
+                else
+                    -- remove the variable
+                    V := VariableStore.From_String(VarString);
+                    VariableStore.Remove(C.variableDB, V);
+                end if;
+            end;
+        end if;
+    end removeVar;
+
+    -- prints out all currently defined variables and their corresponding values.
+    procedure list(C : in MyCalculator) is
+    begin
+        -- check lock state
+        if (C.isLocked = True) then
+            raise Lock_Exception with "Calculator is locked.";
+        else
+            -- print out all currently defined variables and their corresponding values
+            VariableStore.Print(C.variableDB);
+        end if;
+    end list;
 
 end MyCalculator;
--- mycalculator.ads
+++ mycalculator.ads
@@ -9,14 +9,21 @@ generic
 package MyCalculator with SPARK_Mode is
     type MyCalculator is private;
 
+    -- public functions -------------------------------------------------------
+
+    -- initializes the calculator with the given master PIN.
     procedure Init(C : out MyCalculator; MasterPINString : in String);
 
+    -- unlock the calculator with the given PIN.
     procedure Unlock(C : in out MyCalculator; PINString : in String);
 
+    -- lock and update the master pin.
     procedure lock(C : in out MyCalculator; PINString : in String);
 
+    -- check if the calculator is locked or not
     function GetLockState(C : in MyCalculator) return Boolean;
 
+    -- get the size of the stack
     function Size(C : in MyCalculator) return Integer;
 
     -- helper ghost function for checking
@@ -40,14 +47,14 @@ package MyCalculator with SPARK_Mode is
 
     -- For a string var, the procedure loads the value stored 
     -- in variable var and pushes it onto the stack.
-    procedure loadVar(C : in out MyCalculator; Var: in String);
+    procedure loadVar(C : in out MyCalculator; VarString: in String);
 
     -- pops the value from the top of the stack and stores it 
     -- into variable var, defining that variable if it is not already defined.
-    procedure storeVar(C : in out MyCalculator; Var: in String);
+    procedure storeVar(C : in out MyCalculator; VarString: in String);
 
     -- makes variable var undefined (i.e. it will not be printed by subsequent “list” commands).
-    procedure removeVar(C : in out MyCalculator; Var: String);
+    procedure removeVar(C : in out MyCalculator; VarString: String);
 
     -- prints out all currently defined variables and their corresponding values.
     procedure list(C : in MyCalculator);
@@ -63,26 +70,44 @@ private
         variableDB : VariableStore.Database;
         end record;
 
+    -- private Utils ----------------------------------------------------------
+
+    -- help function for getting stack top
     function Size(C : in MyCalculator) return Integer is
         (C.size);
 
+    -- helper function for get the value of the stack storage
     function Storage(C : in MyCalculator; Pos : in Integer) return Item is
         (C.storage(Pos));
 
+    -- helper function for checking if the calculator is locked or not
     function GetLockState(C : in MyCalculator) return Boolean is
         (C.isLocked);
 
+    -- helper function for checking if the string is a valid number
     function IsNumber (S:String) return Boolean is
         (for all I in S'Range => 
             S(I) >= '0' and S(I) <= '9');
    
+    -- check if the string is a valid pin, is a 4-digit string of 
+    -- non-whitespace characters that represents a non-negative number 
+    -- (i.e. a natural number) in the range 0000 . . . 9999. 
     function IsPin (S : String) return Boolean is
         (S'Length = 4 and IsNumber(S));
 
+    -- check if the string is a valid integer
     function IsValidInt(V : Item) return Boolean is
         (V  >= Integer'First and V <= Integer'Last);
 
+    -- check if the string is a valid operator
     function IsValidOperator(S : String) return Boolean is
         (S = "+" or S = "-" or S = "*" or S = "/");
+
+    -- check if the string is a valid Var name, which is the 
+    -- string of non-whitespace characters, and names longer 
+    -- than 1024 characters are invalid.
+    function IsValidVarName(S : String) return Boolean is
+        (S'Length <= 1024 and for all I in S'Range 
+            => S(I) /= ' ');
 
 end MyCalculator;
